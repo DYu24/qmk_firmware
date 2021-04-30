5a432daf71c5171711870449c79c1c7ce8fa6812
Derek Yu <derekyu624@hotmail.com>
2021-04-30 00:32:44 -0400

    Add media keys layer and rgb toggling on/off

---
+++ keyboards/massdrop/ctrl/keymaps/dyu24/keymap.c
@@ -0,0 +1,228 @@
+#include QMK_KEYBOARD_H
+
+enum ctrl_keycodes {
+    U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
+    U_T_AGCR,              //USB Toggle Automatic GCR control
+    RGB_ONF,             //RGB Toggle On/Off
+    DBG_TOG,               //DEBUG Toggle On / Off
+    DBG_MTRX,              //DEBUG Toggle Matrix Prints
+    DBG_KBD,               //DEBUG Toggle Keyboard Prints
+    DBG_MOU,               //DEBUG Toggle Mouse Prints
+    MD_BOOT,               //Restart into bootloader after hold timeout
+};
+
+enum ctrl_rgb_mode {
+    RGB_MODE_ALL,
+    RGB_MODE_KEYLIGHT_AND_MODIFIER,
+    RGB_MODE_UNDERGLOW,
+    RGB_MODE_NONE,
+};
+
+typedef union {
+    uint32_t raw;
+    struct {
+        uint8_t rgb_mode :8;
+        uint8_t last_rgb_mode :8;
+    };
+} ctrl_config_t;
+
+ctrl_config_t ctrl_config;
+
+keymap_config_t keymap_config;
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             KC_PSCR, TG(2), KC_PAUS, \
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP, \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                              KC_UP, \
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(1),   KC_APP,  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT \
+    ),
+    [1] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, \
+        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, _______, _______, _______, _______,   _______, _______, _______, \
+        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______, _______, \
+        _______, RGB_TOG, RGB_ONF, _______, _______, MD_BOOT, NK_TOGG, _______, _______, _______, _______, _______,                              _______, \
+        _______, _______, _______,                   _______,                            _______, _______, _______, _______,            _______, _______, _______ \
+    ),
+    [2] = LAYOUT(
+        _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, KC_MPRV, KC_MNXT, KC_BRID, KC_BRIU, _______, _______, _______, _______,            _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                              _______, \
+        _______, _______, _______,                   _______,                            _______, _______, _______, _______,            _______, _______, _______ \
+    ),
+};
+
+// Runs just one time when the keyboard initializes.
+void matrix_init_user(void) { };
+
+// Runs constantly in the background, in a loop.
+void matrix_scan_user(void) { };
+
+// Keyboard post initialization
+void keyboard_post_init_kb(void) {
+    // Set pre-configured RGB mode
+    ctrl_config.raw = eeconfig_read_user();
+    switch (ctrl_config.rgb_mode) {
+        case RGB_MODE_ALL:
+            rgb_matrix_set_flags(LED_FLAG_ALL);
+            rgb_matrix_enable_noeeprom();
+            break;
+        case RGB_MODE_KEYLIGHT_AND_MODIFIER:
+            rgb_matrix_set_flags(LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER);
+            rgb_matrix_set_color_all(0, 0, 0);
+            break;
+        case RGB_MODE_UNDERGLOW:
+            rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
+            rgb_matrix_set_color_all(0, 0, 0);
+            break;
+        case RGB_MODE_NONE:
+            rgb_matrix_set_flags(LED_FLAG_NONE);
+            rgb_matrix_disable_noeeprom();
+            break;
+    }
+}
+
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//     switch(get_highest_layer(state)) {
+
+//     }
+//     return state;
+// }
+
+#define MODS_SHIFT  (get_mods() & MOD_MASK_SHIFT)
+#define MODS_CTRL   (get_mods() & MOD_MASK_CTRL)
+#define MODS_ALT    (get_mods() & MOD_MASK_ALT)
+
+led_flags_t get_led_flag(uint8_t rgb_mode) {
+    switch (rgb_mode) {
+        case RGB_MODE_ALL:
+            return LED_FLAG_ALL;
+        case RGB_MODE_KEYLIGHT_AND_MODIFIER:
+            return LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER;
+        case RGB_MODE_UNDERGLOW:
+            return LED_FLAG_UNDERGLOW;
+        default:
+            return LED_FLAG_NONE;
+    }
+}
+
+void turn_on_off_rgb(bool turnOn) {
+    if (turnOn) {
+        ctrl_config.rgb_mode = ctrl_config.last_rgb_mode;
+        rgb_matrix_set_flags(get_led_flag(ctrl_config.rgb_mode));
+        rgb_matrix_enable_noeeprom();
+    } else {
+        ctrl_config.rgb_mode = RGB_MODE_NONE;
+        rgb_matrix_set_flags(LED_FLAG_NONE);
+        rgb_matrix_disable_noeeprom();
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static uint32_t key_timer;
+
+    switch (keycode) {
+        case U_T_AUTO:
+            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+                TOGGLE_FLAG_AND_PRINT(usb_extra_manual, "USB extra port manual mode");
+            }
+            return false;
+        case U_T_AGCR:
+            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+                TOGGLE_FLAG_AND_PRINT(usb_gcr_auto, "USB GCR auto mode");
+            }
+            return false;
+        case DBG_TOG:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_enable, "Debug mode");
+            }
+            return false;
+        case DBG_MTRX:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_matrix, "Debug matrix");
+            }
+            return false;
+        case DBG_KBD:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_keyboard, "Debug keyboard");
+            }
+            return false;
+        case DBG_MOU:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_mouse, "Debug mouse");
+            }
+            return false;
+        case MD_BOOT:
+            if (record->event.pressed) {
+                key_timer = timer_read32();
+            } else {
+                if (timer_elapsed32(key_timer) >= 500) {
+                    reset_keyboard();
+                }
+            }
+            return false;
+        case RGB_ONF:
+            if (record->event.pressed) {
+                switch (rgb_matrix_get_flags()) {
+                    case LED_FLAG_ALL:
+                        ctrl_config.last_rgb_mode = RGB_MODE_ALL;
+                        turn_on_off_rgb(false);
+                        break;
+                    case LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER:
+                        ctrl_config.last_rgb_mode = RGB_MODE_KEYLIGHT_AND_MODIFIER;
+                        turn_on_off_rgb(false);
+                        break;
+                    case LED_FLAG_UNDERGLOW:
+                        ctrl_config.last_rgb_mode = RGB_MODE_UNDERGLOW;
+                        turn_on_off_rgb(false);
+                        break;
+                    default:
+                        ctrl_config.rgb_mode = RGB_MODE_ALL;
+                        turn_on_off_rgb(true);
+                        break;
+                }
+                eeconfig_update_user(ctrl_config.raw);
+            }
+            return false;
+        case RGB_TOG:
+            if (record->event.pressed) {
+                switch (rgb_matrix_get_flags()) {
+                    case LED_FLAG_ALL: {
+                        ctrl_config.rgb_mode = RGB_MODE_KEYLIGHT_AND_MODIFIER;
+                        rgb_matrix_set_flags(LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER);
+                        rgb_matrix_set_color_all(0, 0, 0);
+                        break;
+                    }
+                    case LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER: {
+                        ctrl_config.rgb_mode = RGB_MODE_UNDERGLOW;
+                        rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
+                        rgb_matrix_set_color_all(0, 0, 0);
+                        break;
+                    }
+
+                    case LED_FLAG_UNDERGLOW: {
+                        ctrl_config.rgb_mode = RGB_MODE_NONE;
+                        rgb_matrix_set_flags(LED_FLAG_NONE);
+                        rgb_matrix_disable_noeeprom();
+                        break;
+                    }
+                    default: {
+                        ctrl_config.rgb_mode = RGB_MODE_ALL;
+                        rgb_matrix_set_flags(LED_FLAG_ALL);
+                        rgb_matrix_enable_noeeprom();
+                        break;
+                    }
+                }
+                eeconfig_update_user(ctrl_config.raw);
+            }
+            return false;
+        default:
+            return true; //Process all other keycodes normally
+    }
+}
+
